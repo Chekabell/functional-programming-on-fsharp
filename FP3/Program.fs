85797288638581345203d1a030159d227e483f6a
Chekabell <nikita.toporischev@mail.ru>
2025-03-15 14:35:12 +0500

    Third task is completed

--- FP3/Program.fs
+++ FP3/Program.fs
@@ -1,24 +1,24 @@
 ﻿let areEquals (a: float) (b : float) =
     abs(a-b) < 1e-9
 
-let getLengthSide (firstCoord : List<float>) (secondCoord : List<float>) = 
-    sqrt((secondCoord[0] - firstCoord[0]) ** 2 + (secondCoord[1] - firstCoord[1]) ** 2)
+let getLengthSide (x1 : float, y1 : float) (x2 : float, y2 : float) = 
+    sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
-let isEquilateral (coord:List<List<float>>) =
+let isEquilateral (coord : List<(float * float)>) =
     areEquals (getLengthSide coord.[0] coord[1]) (getLengthSide coord.[0] coord[2]) &&
     areEquals (getLengthSide coord.[0] coord[2]) (getLengthSide coord.[1] coord[2])
 
-let isIsosceles (coord: List<List<float>>) =
+let isIsosceles (coord : List<(float * float)>) =
     areEquals (getLengthSide coord.[0] coord[1]) (getLengthSide coord.[0] coord[2])  ||
     areEquals (getLengthSide coord.[0] coord[1]) (getLengthSide coord.[1] coord[2])  ||
     areEquals (getLengthSide coord.[0] coord[2]) (getLengthSide coord.[1] coord[2])
 
-let isRightAngled (coord: List<List<float>>) =
+let isRightAngled (coord : List<(float * float)>) =
     areEquals (getLengthSide coord[0] coord[2] ** 2 + getLengthSide coord[1] coord[2] ** 2) (getLengthSide coord[0] coord[1] ** 2) ||
     areEquals (getLengthSide coord[0] coord[1] ** 2 + getLengthSide coord[1] coord[2] ** 2) (getLengthSide coord[0] coord[2] ** 2) ||
     areEquals (getLengthSide coord[0] coord[1] ** 2 + getLengthSide coord[0] coord[2] ** 2) (getLengthSide coord[1] coord[2] ** 2)
 
-let getTypeTriangle (lst: List<List<float>>) =
+let getTypeTriangle (lst: List<(float * float)>) =
     if isEquilateral lst
     then "равносторонний"
     elif isIsosceles lst && isRightAngled lst
@@ -30,4 +30,4 @@ let getTypeTriangle (lst: List<List<float>>) =
     else "разносторонний"
 
 
-printfn $"Этот треугольник - {getTypeTriangle [[0;0];[3;0];[1;2]]}"
+printfn $"Этот треугольник - {getTypeTriangle [ 0,0; 2,0; 0,2 ]}"
